49e89d1cd6f9cf9321d9d43acf8af0563e72c31c
Kamila <arslanova.25.09.00@gmail.com>
2024-03-14 21:25:44 +0500

    first

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const PORT = process.env.PORT || 4040;
+
+const app = express();
+app.use(express.json());
+app.post("*", async (req, res) => {
+  res.send("Hello post");
+});
+app.get("*", async (req, res) => {
+  res.send("Hello get");
+});
+
+app.listen(PORT, function (err) {
+  if (err) console.log(err);
+  console.log("Server listening on PORT", PORT);
+});
